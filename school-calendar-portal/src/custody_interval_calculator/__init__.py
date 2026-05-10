3b99892c174bc21cb2f887c22f97af771addb376
hanyunfan <frank.han@dell.com>
2026-05-10 10:04:13 -0500

    Delete school-calendar-portal directory

--- school-calendar-portal/src/custody_interval_calculator/__init__.py
+++
@@ -1,7 +0,0 @@
-# custody_interval_calculator
-from .interval_generator import (
-    CustodyInterval, IntervalList, StandardCalendar,
-    SchoolYear, SchoolBreak, NoSchoolDay,
-    CustodyIntervalGenerator,
-    load_calendar, save_intervals
-)
--- school-calendar-portal/src/static_web_generator/__init__.py
+++
@@ -1,2 +0,0 @@
-# static_web_generator
-from .html_builder import HTMLBuilder, I18N
